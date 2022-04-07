bc90d28f15d5c39bff22f5d8d451ed53c5ef2ea9
KohakuOgata <71256071+KohakuOgata@users.noreply.github.com>
2022-04-08 00:18:59 +0900

    Vector2クラス追加

---
+++ MonPazzle/Vector2.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Vector.h"
+class Vector2 :
+    public Vector<double, 2>
+{
+public:
+    Vector2();
+    Vector2(const std::array<double, 2>& c);
+
+    double GetAngle() const;
+
+    static double Dot(const Vector2& l, const Vector2& r);
+    static double Cross(const Vector2& l, const Vector2& r);
+    static double GetCosine(const Vector2& v1, const Vector2& v2);
+    static double GetSine(const Vector2& v1, const Vector2& v2);
+};
+
